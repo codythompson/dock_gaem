3fcf49f84690ff7673c91789e3ed638b630d4b7f
codyThompson <cody.thompson22@gmail.com>
2016-10-16 20:39:48 -0700

    camera now fixes ij coords after a scale

--- camera.js
+++ camera.js
@@ -1,20 +1,44 @@
 (function (global) {
 var Camera = function (options) {
   _.extend(this, _.defaults(options || {}, {
-    map: null,
-    x: 0,
-    y: 0,
-    scale_x: 1,
-    scale_y: 1
+    i: 0,
+    j: 0,
+    map: null
   }));
+  this.ij();
 };
 Camera.prototype = {
   ij: function (i, j) {
+    if (arguments.length === 0) {
+      i = this.i;
+      j = this.j;
+    }
+
+    this.i = i;
+    this.j = j;
     var loc = this.map.get_location(i, j);
-    var x = -loc.x;
-    var y = -loc.y;
+    var x = -(loc.x * this.map.cont.scale.x);
+    var y = -(loc.y * this.map.cont.scale.y);
     this.map.cont.x = x;
     this.map.cont.y = y;
+
+    return {
+      i: i,
+      j: j
+    };
+  },
+  scale: function (x, y) {
+    if (arguments.length === 1) {
+      y = x;
+    }
+
+    this.map.cont.scale.set(x, y);
+    this.ij();
+
+    return {
+      scale_x: this.map.cont.scale.x,
+      scale_y: this.map.cont.scale.y
+    };
   }
 };
 
