8c8d5af716ff5bcc0d1aef790450df73edcb41f2
Cody Thompson <cody.thompson22@gmail.com>
2016-10-29 11:35:11 -0700

    added panel for tile buttons

--- ui_init.js
+++ ui_init.js
@@ -44,8 +44,77 @@ var init_control_selector = function (game, controls) {
   });
   mode_group.select(0);
 
-  game.stage.addChild(left_panel.cont);
+  tile_panel = new UIContainer({
+    x: (window.innerWidth / 2) - 200,
+    visible: true
+  });
+
+  var grass_up = PIXI.Sprite.fromImage('assets/noise_med.png');
+  grass_up.scale.x = 0.5;
+  grass_up.scale.y = 0.5;
+  grass_up.tint = 0xcccc66;
+  grass_up.x = 8;
+  grass_up.y = 8;
+  var grass_skirt = PIXI.Sprite.fromImage('assets/button_up.png');
+  var grass_down = PIXI.Sprite.fromImage('assets/noise_med.png');
+  grass_down.scale.x = 0.5;
+  grass_down.scale.y = 0.5;
+  grass_down.tint = 0xcccc66;
+  grass_down.x = -8;
+  grass_down.y = -8;
+  var grass_button = new UIButton({
+    up_sprites: [grass_up, grass_skirt],
+    down_sprites: [grass_down],
+    x: tile_panel.cont.width / 2,
+    y: 150
+  });
+  tile_panel.add_button(grass_button);
+
+  var water_up = PIXI.Sprite.fromImage('assets/water_med.png');
+  water_up.scale.x = 0.5;
+  water_up.scale.y = 0.5;
+  water_up.x = 8;
+  water_up.y = 8;
+  var water_skirt = PIXI.Sprite.fromImage('assets/button_up.png');
+  var water_down = PIXI.Sprite.fromImage('assets/water_med.png');
+  water_down.scale.x = 0.5;
+  water_down.scale.y = 0.5;
+  water_down.x = -8;
+  water_down.y = -8;
+  var water_button = new UIButton({
+    up_sprites: [water_up, water_skirt],
+    down_sprites: [water_down],
+    x: tile_panel.cont.width / 2,
+    y: 300
+  });
+  tile_panel.add_button(water_button);
+
+  var dock_up = PIXI.Sprite.fromImage('assets/dock_med.png');
+  dock_up.scale.x = 0.5;
+  dock_up.scale.y = 0.5;
+  dock_up.x = 8;
+  dock_up.y = 8;
+  var dock_skirt = PIXI.Sprite.fromImage('assets/button_up.png');
+  var dock_down = PIXI.Sprite.fromImage('assets/dock_med.png');
+  dock_down.scale.x = 0.5;
+  dock_down.scale.y = 0.5;
+  dock_down.x = -8;
+  dock_down.y = -8;
+  var dock_button = new UIButton({
+    up_sprites: [dock_up, dock_skirt],
+    down_sprites: [dock_down],
+    x: tile_panel.cont.width / 2,
+    y: 450
+  });
+  tile_panel.add_button(dock_button);
+
+  // tile_group = new UIButtonGroup({
+  //   buttons: [grass_button]
+  // });
+
+
 };
+
 
 /*******************************************************************************
  * ui init - entry point
