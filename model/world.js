a542f2af20c1d1bee10030e3593760035e9686fa
Cody Thompson <cody.thompson22@gmail.com>
2016-12-05 21:22:18 -0800

    added world data class. untested.

---
+++ model/world.js
@@ -0,0 +1,19 @@
+'use strict';
+
+(function (scope) {
+
+var World = function (init_obj) {
+  init_obj = init_obj || {};
+  init_obj.data_type = init_obj.data_type || 'World';
+  scope.model.DataObject.call(this, init_obj);
+
+  this.floors = this.floors || [];
+
+  this.x = this.x || 0;
+  this.y = this.y || 0;
+  this.scale_x = this.scale_x || 1;
+  this.scale_y = this.scale_y || 1;
+  this.rotation = this.rotation || 0;
+};
+
+})(window.$dock_game);
